cf09857f6373253b931724f506b7bd1a13766d16
Allen Zhao <czhao1@hotmail.co.uk>
2017-02-06 15:48:54 -0500

    init commit of AdvancedCalc project with changed theme

---
+++ AdvancedCalc/app/src/main/java/com/example/cz264/advancedcalc/CalcMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cz264.advancedcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_main);
+    }
+}
